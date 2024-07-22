6b47b4d0f3322980a46760e39f191916fe8d9d13
ThamizhArasanJS200357 <thamizharasan2003@gmail.com>
2024-07-22 13:01:35 +0530

    homepage

--- frontend/src/Components/Pages/Homepage/Homepage.jsx
+++ frontend/src/Components/Pages/Homepage/Homepage.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Navbar from '../../Navigation/Navbar'
+import Herosection from '../../Herosection/Herosection'
+import Features from '../../../Features/Features'
+import CardComponent from '../../Price/CardComponent'
+import CallToAction from '../../CallToAction/CallToAction'
+import Review from '../../Review/Review'
+import Footer from '../../Footer/Footer'
 
 function Homepage() {
   return (
-    <div>Homepage</div>
+    <>
+    <Navbar/>
+    <Herosection/>
+    <Features/>
+    <CardComponent/>
+    <CallToAction/>
+    <Review/>
+    <Footer/>
+    </>
   )
 }
 
